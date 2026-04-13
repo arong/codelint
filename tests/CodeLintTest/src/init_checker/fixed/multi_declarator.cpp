49ad7b11203530b9cf95176bed9345e40c57a062
arong <xurongdong@hotmail.com>
2026-04-14 01:20:26 +0800

    fix: skip clang-format for test fixture files and update whitespace format

--- tests/CodeLintTest/src/init_checker/fixed/multi_declarator.cpp
+++ tests/CodeLintTest/src/init_checker/fixed/multi_declarator.cpp
@@ -2,12 +2,12 @@
 // P0-1: All variables in multi-declarator statements should be checked
 
 void test_multiple_declarators() {
-  int a{}, b{}, c{};          // All three should trigger warnings
+  int a{}, b{}, c{};                // All three should trigger warnings
   double x{1.0}, y{}, z{3.0}; // Only y should trigger warning
-  char *p1{}, *p2{}, *p3{};   // All three should trigger warnings
+  char *p1{}, *p2{}, *p3{};         // All three should trigger warnings
 
   int arr1[5]{}, arr2[10]{}; // Both should trigger C-style array warnings
-  float f1{}, f2{}, f3{};    // Only f2 should trigger warning
+  float f1{}, f2{}, f3{};  // Only f2 should trigger warning
 }
 
 void test_mixed_initialization() {
@@ -16,15 +16,15 @@ void test_mixed_initialization() {
 }
 
 void test_single_declarators() {
-  int single{};      // Should trigger warning
+  int single{};        // Should trigger warning
   int initialized{}; // Should NOT trigger warning
 }
 
 class TestClass {
 public:
   void method() {
-    int m1{}, m2{}, m3{}; // All three should trigger warnings
-    bool b1{true}, b2{};  // Both should trigger warnings
+    int m1{}, m2{}, m3{};     // All three should trigger warnings
+    bool b1{true}, b2{}; // Both should trigger warnings
   }
 };
 
